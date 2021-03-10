1d2f9a2525d1edeb70dc09824e198c665cb91178
GeorgeJ1010 <george.joseph@anywhere.co>
2021-03-10 15:02:19 +0530

    c

--- src/main/java/com/se/user.java
+++ src/main/java/com/se/user.java
@@ -50,7 +50,6 @@ public class user extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
 		String name=request.getParameter("name");
 		int age=Integer.parseInt(request.getParameter("age"));
